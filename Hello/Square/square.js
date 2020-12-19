bb6308726ef18fe243ee6872fc3d70e52b76ac14
Jabaker35 <74387163+Jabaker35@users.noreply.github.com>
2020-12-19 12:30:36 -0500

    update files

--- Hello/Square/square.js
+++ Hello/Square/square.js
@@ -1,3 +1,11 @@
+const randColorRGB = () => {
+  const red = Math.floor( Math.random() * 256);
+  const green = Math.floor(Math.random()* 256);
+  const blue = Math.floor(Math.random()* 256);
+  return "rgb(" + red + "," + green + "," + blue + ")";
+}
+console.log(randColorRGB())
+
 const generateSquares = () => {
   $(()=> {
     for(let i = 1; i <= 1000; i++){
@@ -11,16 +19,6 @@ const generateSquares = () => {
 $square.attr('id', 'square', i);
 }
 
-
 $(() => {
   generateSquares(1000);
 });
-
-const randColorRGB = () => {
-  const red = Math.floor( Math.random() * 256);
-  const green = Math.floor(Math.random()* 256);
-  const blue = Math.floor(Math.random()* 256);
-  return "rgb(" + red + "," + green + "," + blue + ")";
-}
-
-console.log(randColorRGB())

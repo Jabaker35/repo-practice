49a76b69d5d18829af23caee147a262a433e32e6
Jabaker35 <74387163+Jabaker35@users.noreply.github.com>
2020-12-19 15:14:46 -0500

    update main

--- Hello/Promise.js
+++ Hello/Promise.js
@@ -61,3 +61,72 @@ const handleFailure = (rejectReason) => {
 // Write your code below:
 checkInventory(order)
 .then(handleSuccess).catch(handleFailure);
+
+
+const {checkInventory, processPayment, shipOrder} = require('./library.js');
+
+const order = {
+  items: [['sunglasses', 1], ['bags', 2]],
+  giftcardBalance: 79.82
+};
+
+checkInventory(order)
+.then((resolvedValueArray) => {
+  // Write the correct return statement here:
+ return processPayment(resolvedValueArray)
+})
+.then((resolvedValueArray) => {
+  // Write the correct return statement here:
+  return shipOrder(resolvedValueArray)
+})
+.then((successMessage) => {
+  console.log(successMessage);
+})
+.catch((errorMessage) => {
+  console.log(errorMessage);
+});
+
+
+const {checkInventory, processPayment, shipOrder} = require('./library.js');
+
+const order = {
+  items: [['sunglasses', 1], ['bags', 2]],
+  giftcardBalance: 79.82
+};
+
+// Refactor the code below:
+
+checkInventory(order)
+    .then((resolvedValueArray) => {
+        return processPayment(resolvedValueArray)
+       })
+
+    .then((resolvedValueArray) => {
+        return shipOrder(resolvedValueArray)
+       })
+
+    .then((successMessage) => {
+        console.log(successMessage);
+      });
+
+
+      const {checkAvailability} = require('./library.js');
+
+      const onFulfill = (itemsArray) => {
+        console.log(`Items checked: ${itemsArray}`);
+        console.log(`Every item was available from the distributor. Placing order now.`);
+      };
+
+      const onReject = (rejectionReason) => {
+      	console.log(rejectionReason);
+      };
+
+      // Write your code below:
+
+      const checkSunglasses = checkAvailability('sunglasses', 'Favorite Supply Co.');
+      const checkPants = checkAvailability('pants', 'Favorite Supply Co.'); 
+      const  checkBags = checkAvailability('bags', 'Favorite Supply Co.');
+
+      Promise.all([checkSunglasses, checkPants, checkBags])
+        .then(onFulfill)
+        .catch(onReject);

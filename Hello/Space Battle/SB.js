5f4d07c7231a741f7629a37c444e84adf112f887
Jabaker35 <74387163+Jabaker35@users.noreply.github.com>
2021-01-02 15:03:07 -0500

    update

--- Hello/Space Battle/SB.js
+++ Hello/Space Battle/SB.js
@@ -7,8 +7,8 @@ class SpaceShip {
     this.accuracy = accuracy;
   }
 }
-
-console.log('%c Welcome to Space Battle', 'font-size:40px');
+//Welcome header to the player
+console.log('%c Welcome to Space Battle', 'text-decoration: underline; font-size:40px; color: violet');
 
 //Specifics of the players spaceship
 let player = new SpaceShip (20, 'USS Schwarzenegger', 5, 0.7)
@@ -26,32 +26,32 @@ for (let ship = 1; ship <= 6; ship++) {
     var alienAccuracy = (Math.floor(Math.random()*3)+6)/10;
     let alien = new SpaceShip (alienHull, 'alien', alienFirePower, alienAccuracy)
     battle.push(alien);
-};
+}; //Allows the player to fight each alien one at a time with random alien spaceships
 console.log(battle);
 
 //Loops through the battle until player wins or looses
 while (battle.length > 0) {
   let remain = battle.pop();
   if (Math.random() < remain.accuracy) {
-    console.log("%c Mayday Captain, we've been hit!", 'font-weight: bold; color: red;');
+    console.log("%c Mayday Captain, we've been hit!", "font-weight: bold; font-size: 15px; font-variant: small-caps; color: red");
     player.hull = player.hull - remain.firepower;
     console.log(player.hull);
   } else if (Math.random() > remain.accuracy) {
-    console.log("%c Congradulations Captain, we hit target " + player.firepower + " damage!", 'font-style: italic; background: azure; border: 1px solid green;');
+    console.log("%c Congradulations Captain, we hit target " + player.firepower + " damage!", "font-style: italic; font-variant: small-caps; text-transform: capitalize; font-size: 15px; background: azure; border: 1px solid green");
     console.log(remain.hull - player.firepower);
-  } console.log("%c Great Job Captain, Alien ship destroyed!", 'font-weight: bold; color: blue;');
-  //Checks the player's health
+  } console.log("%c Great Job Captain, Alien ship destroyed!", "font-weight: bold; font-variant: small-caps; font-size: 15px; font-variant: small-caps; color: blue");
+  //Checks the player's health (hull) to see it has reached 0 before continuing game
   if (player.hull <= 0) {
-    console.log("%c We did our best captain!", 'font-weight: bold; font-color: grey');
+    console.log("%c We did our best captain!", "font-weight: bold; font-variant: small-caps; font-size: 15px; background: azure; border: 1px solid red; color: orange");
     break;
   }; //Confirm if the player wants to continue the same battle
-  if (!confirm("Continue: Y/N?")) {
-    console.log("%c Coward, I guess they'll die another day!", 'font-weight: bold; color: yellow;');
+  if (!confirm("Continue: Yes/No?")) {
+    console.log("%c Coward, Why run from a winning battle!", "font-weight: bold; font-size: 15px; font-variant: small-caps; color: grey");
     break;
   }
 };
 
 //Signals player the game is over if they won
 if (player.hull > 0 && battle.length == 0) {
-  console.log("%c Awesome Job Captian, all Alien ships have been destroyed!", 'font-style: bold; font-color: solid brown;');
+  console.log("%c Awesome Job Captian, all Alien ships have been destroyed!", "background: azure; border: 1px solid grey; text-transform: capitalize; font-weight: bold; font-style: italic; font-variant: small-caps; color: indigo; font-size: 15px");
 }
